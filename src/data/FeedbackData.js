95f5acc127a46df77acad812b4e75477e8d5428a
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-03-02 13:45:10 +0000

    First 5 sections of Traversy React Course

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, facere repellat? Nostrum nulla doloremque unde possimus? Maxime sunt error itaque. Explicabo exercitationem est laboriosam enim quas obcaecati? Amet, architecto aspernatur.",
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, facere repellat? Nostrum nulla doloremque unde possimus? Maxime sunt error itaque. Explicabo exercitationem est laboriosam enim quas obcaecati? Amet, architecto aspernatur.",
+    },
+    {
+        id: 3,
+        rating: 6,
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, facere repellat? Nostrum nulla doloremque unde possimus? Maxime sunt error itaque. Explicabo exercitationem est laboriosam enim quas obcaecati? Amet, architecto aspernatur.",
+    },
+];
+
+export default FeedbackData;
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,22 @@
+import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
+
+const AboutPage = () => {
+    return (
+        <Card>
+            <div className="about">
+                <h1>About This Project</h1>
+                <p>
+                    This is a React app to elave feedback for a product or
+                    service
+                </p>
+                <p>Version: 1.0.0</p>
+                <p>
+                    <Link to='/'>Back Home</Link>
+                </p>
+            </div>
+        </Card>
+    );
+};
+
+export default AboutPage;
