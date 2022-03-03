ec487042f97674cc0d9a2ef3163e8f10b3f3fe1d
franciskershawfc <99820703+franciskershawfc@users.noreply.github.com>
2022-03-03 09:50:27 +0000

    wire CRUD functions to mock backend, complete walkthrough

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,16 +1,20 @@
-import { motion, AnimatePresence } from "framer-motion";
-import { useContext } from "react";
-import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import FeedbackItem from './FeedbackItem';
 import FeedbackContext from '../context/FeedbackContext';
+import Spinner from './shared/Spinner';
 
 const FeedbackList = () => {
-    const {feedback} = useContext(FeedbackContext)
+    const { feedback, isLoading } = useContext(FeedbackContext);
 
-    if (!feedback || feedback.length === 0) {
+    if (!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No Feedback Yet</p>;
     }
-    return (
-        <div className="feedback-list">
+
+    return isLoading ? (
+        <Spinner />
+    ) : (
+        <div className='feedback-list'>
             <AnimatePresence>
                 {feedback.map((item) => (
                     <motion.div
@@ -19,10 +23,7 @@ const FeedbackList = () => {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                     >
-                        <FeedbackItem
-                            key={item.id}
-                            item={item}
-                        />
+                        <FeedbackItem key={item.id} item={item} />
                     </motion.div>
                 ))}
             </AnimatePresence>
@@ -30,4 +31,4 @@ const FeedbackList = () => {
     );
 };
 
-export default FeedbackList
+export default FeedbackList;
